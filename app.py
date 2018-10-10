ce147630153edcfd9315d1c7a16fa33604898bd3
dean <dlyoung119@me.com>
2018-10-09 18:48:48 -0700

    move secret

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import requests
 from requests_oauthlib import OAuth2Session
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 # This information is obtained upon registration of a new Awair OAuth
 # application at https://developer.getawair.com
@@ -145,5 +146,4 @@ if __name__ == "__main__":
 	# This allows us to use a plain HTTP callback
 	os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
 	
-	app.secret_key = os.urandom(24)
 	app.run(debug=True)
