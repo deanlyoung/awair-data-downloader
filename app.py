9b98e1e4b40b4a094ba09ddbfb424b7c27cc2675
dean <dlyoung119@me.com>
2018-10-11 15:35:17 -0700

    test

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def callback():
 	oauth = OAuth2Session(client_id, redirect_uri=redirect_uri)
 	token = oauth.fetch_token(token_url, client_id=client_id, client_secret=client_secret, authorization_response=request.url)
 	
-	return jsonify(token)
+	return "test: " + jsonify(token)
 	
 	# We use the session as a simple DB for this example.
 	session['oauth_token'] = token['access_token']
