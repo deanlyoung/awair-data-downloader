4cb8d23f07195250846527787a4d43900c85b7bb
dean <dlyoung119@me.com>
2018-10-11 14:30:19 -0700

    authorization_url

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def callback():
 	"""
 	
 	oauth = OAuth2Session(client_id, redirect_uri=redirect_uri)
-	token = oauth.fetch_token(token_url, client_secret=client_secret, authorization_response=oauth.url)
+	token = oauth.fetch_token(token_url, client_secret=client_secret, authorization_response=oauth.authorization_url)
 	
 	# We use the session as a simple DB for this example.
 	session['oauth_token'] = token['access_token']
